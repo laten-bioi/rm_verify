04330b65da6f02a656bb0553fd564c7ddf411edc
EthanHolleman <eholleman@luc.edu>
2020-02-10 16:10:20 -0600

    brainstorm possible args

--- args.py
+++ args.py
@@ -3,4 +3,14 @@ import argparse
 '''
 This file will be the argparse file once things get more organized into a
 complete pipeline with command line interface.
+
+Potential args
+--------------------------------------------
+
+-i: location of seperated family files from one superfam
+-o: dir where to write output may need to make sub dirs in here
+    for different kinds of output
+-b: path to blast database
+-t: number of threads to use
+
 '''
