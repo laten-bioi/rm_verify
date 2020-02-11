1a7fc2d9d19fbc89a6e7774dbff5f07f17d26d43
EthanHolleman <eholleman@luc.edu>
2020-02-10 19:09:35 -0600

    added write_from_tuples wrapper funcion to write rep_seq tuples

--- fasta_io.py
+++ fasta_io.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import os
+from fasta_tools import write_from_tuple_list
 '''
 Function in this file are for reading from input files and writing program
 output to output files
@@ -19,3 +20,6 @@ def read_family_files(super_fam_dir):
             file_paths.append(os.path.join(super_fam_dir, ff))
 
     return file_paths
+
+def write_rep_tuples_to_fasta(rep_seq_tuples, output_file):
+    write_from_tuple_list(rep_seq_tuples, output_file)
