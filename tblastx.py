a2ee63b9a533f299ef1c1205085cc92f9d47d3b5
EthanHolleman <eholleman@luc.edu>
2020-02-10 19:08:44 -0600

    add draft tblastx command caller

--- tblastx.py
+++ tblastx.py
@@ -12,8 +12,17 @@ tblastx -db [database name] -query [input file name]
 -out outfile name
 '''
 
-def run_tblastx(blastdb, query, evalue=10):
-    pass
+
+def run_tblastx(blastdb, query, evalue=10, threads=1, outfmt=10):
+    # outfmt 10 for comma seperated file
+    cmd = ['tblastx', '-query', query, '-db', blastdb, '-evalue', evalue,
+           '-num_threads' threads, '-outfmt', 10]
+    # command still needs testing
+    call = subprocess.call(cmd, shell=True)
+
+    return call
+
+
 
 def count_tblastx_hits(tblastx_output):
     pass
