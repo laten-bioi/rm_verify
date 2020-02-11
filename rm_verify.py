09dff46d2863b9c897d4d032bf9745e78455d2a8
EthanHolleman <eholleman@luc.edu>
2020-02-10 19:09:13 -0600

    write rep seqs to fasta file so they can be handed to tblastx function

--- rm_verify.py
+++ rm_verify.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import sys
 from args import get_args
+
 from big_hitter import get_rep_sequences_from_family_files
 from big_hitter import run_from_clstrs
+from fasta_io import write_rep_tuples_to_fasta
 
 # would be good to add option to start the run from the clstr files
 # so dont need to redo that everytime when testing
@@ -13,12 +15,14 @@ def main():
     rep_seqs = []
     if run_args.c:  # run from collection of clstr files
         rep_seqs = run_from_clstrs(run_args.c)
-        print(rep_seqs[0])
     else:  # run for the first time
         rep_seqs = get_rep_sequences_from_family_files(run_args.i, run_args.o)
 
+    rep_fasta_file = os.path.join(run_args.o, 'rep_seqs.fa')
+    write_rep_tuples_to_fasta(rep_seqs, rep_fasta_file)
+
     # if reached this point means that we have the representative seqs
-    # in a list of tuples. Now need to do the blasting from here
+    # in a file. Now need to do the blasting from here
 
     # STEP 2: Run tblastx on all rep sequences
     # STEP 3: Count total hits (check redundant?)
